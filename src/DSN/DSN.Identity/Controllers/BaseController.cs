601afed4cde9882c72d39965c073e34bdd956d4d
dmitriy <dimonslayerrooock@mail.ru>
2019-11-19 19:57:55 +0300

    фыв

---
+++ src/DSN/DSN.Identity/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DSN.Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected bool IsAdming => User.IsInRole("SomeRole");
+
+        protected Guid UserId => string.IsNullOrWhiteSpace(User?.Identity?.Name) ? Guid.Empty : Guid.Parse(User.Identity.Name);
+    }
+}
---
+++ src/DSN/DSN.Identity/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DSN.Identity.Controllers
+{
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Identity Service Works");
+    }
+}
