6a6230881fd8c5bb875fcce357060997e5c93535
DmitryYavorsky <dyavorksy@elinext.com>
2019-11-26 17:56:48 +0300

    fill paged query

--- src/DSN/DSN.Common/Types/IPagedQuery.cs
+++ src/DSN/DSN.Common/Types/IPagedQuery.cs
@@ -6,5 +6,9 @@ namespace DSN.Common.Types
 {
     public interface IPagedQuery: IQuery
     {
+        int Page { get;  }
+        int Results { get; }
+        string OrderBy { get; }
+        string SortBy { get; }
     }
 }
